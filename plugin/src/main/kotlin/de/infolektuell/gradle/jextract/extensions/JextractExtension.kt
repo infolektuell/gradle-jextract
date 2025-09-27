14846a28004134e762bb5ca8bcde8d5888c32a86
Tamara Cook <info@infolektuell.de>
2025-09-27 23:32:36 +0200

    DSL extension: Add more configuration helpers

--- plugin/src/main/kotlin/de/infolektuell/gradle/jextract/extensions/JextractExtension.kt
+++ plugin/src/main/kotlin/de/infolektuell/gradle/jextract/extensions/JextractExtension.kt
@@ -3,10 +3,12 @@ package de.infolektuell.gradle.jextract.extensions
 import de.infolektuell.gradle.jextract.tasks.JextractBaseTask.*
 import org.gradle.api.Action
 import org.gradle.api.NamedDomainObjectContainer
+import org.gradle.api.file.Directory
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
+import org.gradle.jvm.toolchain.JavaLanguageVersion
 import javax.inject.Inject
 
 abstract class JextractExtension @Inject constructor(private val objects: ObjectFactory) {
@@ -17,10 +19,29 @@ abstract class JextractExtension @Inject constructor(private val objects: Object
     fun download(action: Action<in RemoteJextractInstallation>) {
         installation.set(objects.newInstance(RemoteJextractInstallation::class.java, action))
     }
+    fun download(javaLanguageVersion: JavaLanguageVersion) {
+        download {
+            it.javaLanguageVersion.convention(javaLanguageVersion)
+        }
+    }
 
     /** Configures a local Jextract installation to be used by the plugin */
     fun local(action: Action<in LocalJextractInstallation>) {
         installation.set(objects.newInstance(LocalJextractInstallation::class.java, action))
+    }
+
+    /** Configures a local Jextract installation to be used by the plugin */
+    fun local(location: DirectoryProperty) {
+        local {
+            it.location.convention(location)
+        }
+    }
+
+    /** Configures a local Jextract installation to be used by the plugin */
+    fun local(location: Directory) {
+        local {
+            it.location.convention(location)
+        }
     }
 
     /** A [properties][java.util.Properties] file containing the remote locations where to download the Jextract distributions */
