be916a16e048033d70db66a5f61616a64d72493a
Tamara Cook <info@infolektuell.de>
2025-11-26 17:31:40 +0100

    Migrate model classes to Java

---
+++ plugin/src/main/java/de/infolektuell/gradle/jextract/model/PropertiesCache.java
@@ -0,0 +1,47 @@
+package de.infolektuell.gradle.jextract.model;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+class PropertiesCache {
+    private Properties defaultProperties;
+    private final Map<Path, Properties> data = new HashMap<>();
+    private final String defaultResource;
+
+    PropertiesCache(String defaultResource) {
+        super();
+        this.defaultResource = defaultResource;
+    }
+
+    private Properties getDefaultProperties() {
+        if (defaultProperties != null) {
+            return defaultProperties;
+        }
+        try (InputStream s = PropertiesCache.class.getResourceAsStream(defaultResource)) {
+            var properties = new Properties();
+            properties.load(s);
+            defaultProperties = properties;
+            return defaultProperties;
+        } catch (Exception e) {
+            throw new RuntimeException("Couldn't load default distributions from resource");
+        }
+    }
+    Properties getProperties(Path path) {
+        if (path == null) {
+            return getDefaultProperties();
+        }
+        return data.computeIfAbsent(path, k -> {
+            try (var f = Files.newBufferedReader(k)) {
+                var p = new Properties(getDefaultProperties());
+                p.load(f);
+                return p;
+            } catch(Exception e) {
+                throw new RuntimeException("Couldn't load properties from path");
+            }
+        });
+    }
+}
